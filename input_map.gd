ce7a3c6a05d25c40b41c3bc308b65f15d0f9d75a
kamdee1 <kamdee444@gmail.com>
2026-07-06 23:55:49 +0700

    Add input map configuration script

---
+++ input_map.gd
@@ -0,0 +1,14 @@
+# Input map configuration for Die Hard game
+# Add this to Project Settings > Input Map in Godot
+
+# Standard actions are already defined (ui_up, ui_down, ui_left, ui_right, ui_accept, ui_cancel, ui_select)
+# Additional actions needed:
+# - ui_1 (key: 1)
+# - ui_2 (key: 2)
+# - ui_3 (key: 3)
+
+# To manually add in Godot:
+# 1. Open Project > Project Settings > Input Map
+# 2. Add new action "ui_1" with key "1"
+# 3. Add new action "ui_2" with key "2"
+# 4. Add new action "ui_3" with key "3"
